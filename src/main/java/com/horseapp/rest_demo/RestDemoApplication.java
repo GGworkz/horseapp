b145220c5459f84d9df5262a200e344f9cbabb61
Chris <jctoscano1@gmail.com>
2025-02-25 22:25:36 -0800

    Added bucket4j package to do rate limiting on VeterinarianController

--- src/main/java/com/horseapp/rest_demo/RestDemoApplication.java
+++ src/main/java/com/horseapp/rest_demo/RestDemoApplication.java
@@ -1,10 +1,7 @@
 package com.horseapp.rest_demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RestDemoApplication {
