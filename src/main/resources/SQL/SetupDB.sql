269be5d71ff3df1ac7ff00e0ad826a7df675e11f
Jose Azuara <joseag312@gmail.com>
2025-03-03 10:16:35 -0800

    Fixed DB schema part 2

--- src/main/resources/SQL/SetupDB.sql
+++ src/main/resources/SQL/SetupDB.sql
@@ -1,3 +1,4 @@
+-- Users
 CREATE SEQUENCE IF NOT EXISTS user_seq START 1;
 CREATE TABLE users
 (
@@ -10,6 +11,7 @@ CREATE TABLE users
     phone      VARCHAR(15)  NOT NULL
 );
 
+-- Clients
 CREATE SEQUENCE IF NOT EXISTS client_seq START 1;
 CREATE TABLE clients
 (
@@ -25,6 +27,7 @@ CREATE TABLE clients
         ON UPDATE CASCADE
 );
 
+-- Horses
 CREATE SEQUENCE IF NOT EXISTS horse_seq START 1;
 CREATE TABLE horses
 (
@@ -38,6 +41,7 @@ CREATE TABLE horses
         ON UPDATE CASCADE
 );
 
+-- Consultations
 CREATE SEQUENCE IF NOT EXISTS consultation_seq START 1;
 CREATE TABLE consultations
 (
@@ -51,33 +55,56 @@ CREATE TABLE consultations
         ON UPDATE CASCADE
 );
 
-
-CREATE SEQUENCE IF NOT EXISTS product_type_seq START 1;
-CREATE TABLE product_types
+-- Product Catalogs
+CREATE SEQUENCE IF NOT EXISTS product_catalog_seq START 1;
+CREATE TABLE product_catalogs
 (
-    id   BIGINT PRIMARY KEY DEFAULT NEXTVAL('product_type_seq'),
-    type VARCHAR(50) NOT NULL
-);
-
-CREATE SEQUENCE IF NOT EXISTS consultation_detail_seq START 1;
-CREATE TABLE consultationdetail
-(
-    consultationid BIGINT NOT NULL,
-    productid      BIGINT NOT NULL,
-    quantity       INT    NOT NULL DEFAULT 1,
-    PRIMARY KEY (consultationid, productid),
-    FOREIGN KEY (consultationid)
-        REFERENCES consultation (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    FOREIGN KEY (productid)
-        REFERENCES producttype (id)
+    id        BIGINT DEFAULT NEXTVAL('product_catalog_seq'),
+    name      VARCHAR(50)    NOT NULL,
+    type      VARCHAR(20)    NOT NULL,
+    price     NUMERIC(10, 2) NOT NULL,
+    client_id BIGINT         NOT NULL,
+    PRIMARY KEY (id, client_id),
+    FOREIGN KEY (client_id)
+        REFERENCES clients (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
-CREATE INDEX idx_client_veterinarian_id ON client (veterinarianid);
-CREATE INDEX idx_horses_client_id ON horses (clientid);
-CREATE INDEX idx_consultation_horse_id ON consultation (horseid);
-CREATE INDEX idx_consultation_detail_consultation_id ON consultationdetail (consultationid);
-CREATE INDEX idx_consultation_detail_product_id ON consultationdetail (productid);
+-- Consultation Details
+CREATE TABLE consultation_details
+(
+    consultation_id BIGINT NOT NULL,
+    horse_id        BIGINT NOT NULL,
+    product_id      BIGINT NOT NULL,
+    client_id       BIGINT NOT NULL,
+    quantity        INT    NOT NULL DEFAULT 1,
+    PRIMARY KEY (consultation_id, horse_id, product_id, client_id),
+    FOREIGN KEY (consultation_id, horse_id)
+        REFERENCES consultations (id, horse_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (product_id, client_id)
+        REFERENCES product_catalogs (id, client_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Commonly looked up fields
+CREATE INDEX idx_users_username ON users (username);
+CREATE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_users_phone ON users (phone);
+CREATE INDEX idx_clients_email ON clients (email);
+CREATE INDEX idx_clients_phone ON clients (phone);
+CREATE INDEX idx_consultations_timestamp ON consultations (timestamp);
+CREATE INDEX idx_product_catalogs_type ON product_catalogs (type);
+
+-- Foreign keys indexing to improve JOINS
+CREATE INDEX idx_clients_user_id ON clients (user_id);
+CREATE INDEX idx_horses_client_id ON horses (client_id);
+CREATE INDEX idx_consultations_horse_id ON consultations (horse_id);
+CREATE INDEX idx_product_catalogs_client_id ON product_catalogs (client_id);
+CREATE INDEX idx_consultation_details_consultation_id ON consultation_details (consultation_id);
+CREATE INDEX idx_consultation_details_horse_id ON consultation_details (horse_id);
+CREATE INDEX idx_consultation_details_product_id ON consultation_details (product_id);
+CREATE INDEX idx_consultation_details_client_id ON consultation_details (client_id);
