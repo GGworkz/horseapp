4c9c71515d8b9d628dd7c0349b5c29092da7c8b0
chris <jctoscano1@gmail.com>
2025-03-03 16:34:41 -0800

    dummy commit

--- src/main/java/com/horseapp/config/SecurityConfig.java
+++ src/main/java/com/horseapp/config/SecurityConfig.java
@@ -1,14 +1,14 @@
-//package com.horseapp.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//
-//@Configuration
-//public class SecurityConfig {
-//
+// package com.horseapp.config;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.config.http.SessionCreationPolicy;
+
+// @Configuration
+// public class SecurityConfig {
+
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http
@@ -28,7 +28,7 @@
 //                        .logoutUrl("/user/logout")
 //                        .logoutSuccessUrl("/user/signin")
 //                );
-//
+
 //        return http.build();
 //    }
-//}
+// }
